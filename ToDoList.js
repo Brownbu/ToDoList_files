b0c4d43417373dc1455932275a8fccfcd545272b
Oghenemine <113887335+Brownbu@users.noreply.github.com>
2024-01-31 12:31:25 +0100

    Add files via upload

---
+++ ToDoList.js
@@ -0,0 +1,93 @@
+function interaction1(){
+    var a = document.getElementById("innerEditDelete1");
+    if(a.style.display === "none"){
+        a.style.display = "flex";
+    }
+    else {
+        a.style.display = "none"
+    }
+}
+
+
+function interaction2(){
+    var b = document.getElementById("innerEditDelete2");
+    if(b.style.display === "none"){
+        b.style.display = "flex";
+    }
+    else {
+        b.style.display = "none"
+    }  
+}
+
+ 
+function interaction3(){
+    var c = document.getElementById("innerEditDelete3");
+    if(c.style.display === "none"){
+        c.style.display = "flex";
+    }
+    else {
+        c.style.display = "none"
+    }
+}
+  
+function interaction4(){
+    var d = document.getElementById("innerEditDelete4");
+    if(d.style.display === "none"){
+        d.style.display = "flex";
+    }
+    else {
+        d.style.display = "none"
+    }
+}
+  
+
+function interaction5(){
+    var e = document.getElementById("innerEditDelete5");
+    if(e.style.display === "none"){
+        e.style.display = "flex";
+    }
+    else {
+       e.style.display = "none"
+    }
+
+}
+
+
+function interaction6(){
+    var f = document.getElementById("innerEditDelete6");
+    if(f.style.display === "none"){
+        f.style.display = "flex";
+    }
+    else {
+        f.style.display = "none"
+    }
+}
+
+
+
+function clearField1() {
+    document.getElementById("input1").value='';
+    }
+
+function clearField2() {
+        document.getElementById("input2").value='';
+    }
+
+function clearField3() {
+    document.getElementById("input3").value='';
+    }
+function clearField4() {
+    document.getElementById("input4").value='';
+    }
+function clearField5() {
+    document.getElementById("input5").value='';
+    }
+function clearField6() {
+    document.getElementById("input6").value='';
+    }
+
+
+    
+function scheduleTimer(){
+ 
+}
